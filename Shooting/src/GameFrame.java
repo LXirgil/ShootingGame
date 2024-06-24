a358b8f37726eb36041a739157e3e0ccf8e1f66b
LXirgil <hikkyhachiman0920@gmail.com>
2024-06-24 10:16:16 +0900

    12-1.2

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -14,13 +14,8 @@ public class GameFrame extends MyFrame{
 			
 			movePlayerBullets();
 			moveEnemies();
-			for(int i=0;i<GameWorld.enemies.size();i++) {
-				Enemy e=GameWorld.enemies.get(i);
-				if(e.x==GameWorld.player.x&&e.y==GameWorld.player.y) {
-					System.out.println("やられた!");
-				}
-			}
-			
+			checkPlayerAndEnemies();
+			checkPlayerBulletsAndEnemies();
 			sleep(0.033);
 		}
 	}
@@ -48,5 +43,39 @@ public class GameFrame extends MyFrame{
 			e.move();
 		}
 	}
+	public void checkPlayerAndEnemies() {		
+		for(int i=0;i<GameWorld.enemies.size();i++) {
+			Enemy e=GameWorld.enemies.get(i);
+			if(Math.abs(e.x-GameWorld.player.x)<=30 && 
+					Math.abs(e.y-GameWorld.player.y)<=30) {
+				System.out.println("やられた!");
+				GameWorld.player.y=-1000;
+			}
+		}
+	}
+	public void checkPlayerBulletsAndEnemies() {
+        int i=0;
+        while (i<GameWorld.playerBullets.size()) {
+            PlayerBullet b=GameWorld.playerBullets.get(i);
+            int j=0;
+            int hits=0;
+            while (j<GameWorld.enemies.size()) {
+                Enemy e=GameWorld.enemies.get(j);
+                if (Math.abs(e.x-b.x)<=30&&Math.abs(e.y-b.y)<=30) {
+                    System.out.println("あたり");
+                    hits++;
+                    GameWorld.enemies.remove(j);
+                } else {
+                    j++;
+                }
+            }
+            if (hits>0) {
+                GameWorld.playerBullets.remove(i);
+            } else {
+                i++;
+            }
+        }
+    }
+	
 	
 }
